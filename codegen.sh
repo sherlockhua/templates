36e5ce84cb0ebab1442472982126617f67723673
sherlockhua <sherlockhua@163.com>
2025-02-15 23:48:43 +0800

    add relative code dir

--- codegen.sh
+++ codegen.sh
@@ -3,19 +3,24 @@
 # 初始化变量
 module_name=""
 idl_filename=""
+relative_path=""
+output_dir=""
+source_dir=""
 default_module_name="internal_default_go_module_name"
+default_source_dir="internal_default_go_src_dir"
 need_help=false
 # 定义帮助信息输出函数
 print_help() {
     echo "使用说明:"
-    echo "  $0 [-m|--module <模块名>] [-i|--idl <IDL 文件名>] [-h|--help]"
+    echo "  $0 [-m|--module <模块名>] [-i|--idl <IDL 文件名>] [-r|--relative <生成代码的相对位置>]  [-h|--help]"
     echo "选项:"
     echo "  -m, --module <模块名>    指定模块名"
     echo "  -i, --idl <IDL 文件名>   指定 IDL 文件名，此选项为必需选项"
+    echo "  -r, --relative <生成代码的相对位置>   指定生成代码的相对路径，默认当前位置，比如指定 server/http_server, 生成代码的位置是./server/http_server/"
     echo "  -h, --help               显示此帮助信息"
 }
 # 使用 getopt 解析选项
-ARGS=$(getopt -o m:i:h --long module:,idl:,help -n "$0" -- "$@")
+ARGS=$(getopt -o m:i:o:h --long module:,idl:,output:,help -n "$0" -- "$@")
 if [ $? -ne 0 ]; then
     echo "选项解析失败" >&2
     exit 1
@@ -34,6 +39,11 @@ while true; do
         -i|--idl)
             echo "选项 -i/--idl 被指定，参数值为: $2"
             idl_filename="$2"
+            shift 2
+            ;;
+        -r|--relative)
+            echo "选项 -r/--relative 被指定，参数值为: $2"
+            relative_path="$2"
             shift 2
             ;;
         -h|--help)
@@ -63,21 +73,32 @@ if [ -z "$idl_filename" ]; then
     print_help
     exit 1
 fi
+
+output_dir="$module_name"
+module_path=$module_name
+
+if [ ! -z "$relative_path" ]; then
+    output_dir="$module_name/$relative_path"
+    module_path=$output_dir
+    source_dir="$module_path"
+fi
+
 echo "module_name: $module_name"
 echo "idl_filename: $idl_filename"
+echo "output_dir: $output_dir"
 
 #backup impl.go
-if [ -f "$module_name/go/impl.go" ]; then
-   mv "$module_name/go/impl.go" "$module_name/go/impl.go.bak"
+if [ -f "$output_dir/go/impl.go" ]; then
+   mv "$output_dir/go/impl.go" "$output_dir/go/impl.go.bak"
 fi
 
-openapi-generator generate --template-dir ./  -g go-server  -i "$idl_filename" -o "$module_name"
+openapi-generator generate --template-dir ./  -g go-server  -i "$idl_filename" -o "$output_dir"
 
-if [ -f "$module_name/go/impl.go.bak" ]; then
-   mv "$module_name/go/impl.go.bak" "$module_name/go/impl.go"
+if [ -f "$output_dir/go/impl.go.bak" ]; then
+   mv "$output_dir/go/impl.go.bak" "$output_dir/go/impl.go"
 fi
 
-rm -rf $module_name/go/*_service.go
+rm -rf $output_dir/go/*_service.go
 cp -r configs $module_name/
 
 cp -r script/Makefile $module_name/
@@ -85,12 +106,18 @@ mkdir -p $module_name/script
 cp script/start.sh $module_name/script/
 cp -r script/build.sh $module_name/script/
 
-sed -i '' "s#github.com/GIT_USER_ID/GIT_REPO_ID#$module_name#g" "./$module_name/main.go"
+if [ "$output_dir" != "$module_name" ]; then
+    mv "./$output_dir/go.mod" "./$module_name/"
+fi
+
+
+sed -i '' "s#github.com/GIT_USER_ID/GIT_REPO_ID#$module_path#g" "./$output_dir/main.go"
 sed -i '' "s#$default_module_name#$module_name#g" "./$module_name/go.mod"
 
 for file in ./$module_name/script/*.sh; do
     if [ -f "$file" ]; then
         sed -i '' "s#$default_module_name#$module_name#g" "$file"
+        sed -i '' "s#$default_source_dir#$source_dir#g" "$file"
         chmod +x "$file"
     fi
 done
--- script/build.sh
+++ script/build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 MODULE_NAME=internal_default_go_module_name
+SOURCE_DIR=internal_default_go_src_dir
 if [ ! -e ./go.mod ];then
     go mod init
 fi
 
 go mod tidy
 
-go build -o output/bin/$MODULE_NAME
+go build -o output/bin/$MODULE_NAME $SOURCE_DIR
 cp -r configs output/
 cp ./script/start.sh ./output/
 chmod +x ./output/start.sh
